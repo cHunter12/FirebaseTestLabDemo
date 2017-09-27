ef6b359ee1f085314b350da689a464810b157a02
cHunter12 <christopher.hunter@unosquare.com>
2017-09-27 14:09:40 +0100

    Removed Test Butler, pretty sure it wasn't working Added an activity lifecycle monitory registry to set flags on new activities

--- app/src/androidTest/java/com/example/android/firebasetestlabexample/MockTestRunner.java
+++ app/src/androidTest/java/com/example/android/firebasetestlabexample/MockTestRunner.java
@@ -1,25 +1,33 @@
 package com.example.android.firebasetestlabexample;
 
+import static android.view.WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD;
+import static android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+import static android.view.WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+import static android.view.WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON;
+
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnitRunner;
-
-import com.linkedin.android.testbutler.TestButler;
+import android.support.test.runner.lifecycle.ActivityLifecycleCallback;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import android.support.test.runner.lifecycle.Stage;
 
 public class MockTestRunner extends AndroidJUnitRunner {
 
     @Override
-    public void onStart() {
-        TestButler.setup(InstrumentationRegistry.getTargetContext());
-        super.onStart();
-    }
+    public void onCreate(Bundle arguments) {
+        super.onCreate(arguments);
 
-    @Override
-    public void finish(int resultCode, Bundle results) {
-        TestButler.teardown(InstrumentationRegistry.getTargetContext());
-        super.finish(resultCode, results);
+        ActivityLifecycleMonitorRegistry.getInstance().addLifecycleCallback(new ActivityLifecycleCallback() {
+            @Override
+            public void onActivityLifecycleChanged(Activity activity, Stage stage) {
+                if (stage == Stage.PRE_ON_CREATE) {
+                    activity.getWindow().addFlags(FLAG_SHOW_WHEN_LOCKED | FLAG_TURN_SCREEN_ON | FLAG_KEEP_SCREEN_ON);
+                }
+            }
+        });
     }
 
     @Override
